1bc0a1ae0fd221c4093d22d1e07d7d3f54622a3d
Githamza <hamza.haddad.dev@gmail.com>
2017-08-09 00:58:46 +0200

    change directory name

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'client/portfolio')));
+app.use(express.static(path.join(__dirname, 'client/plannerfront')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
